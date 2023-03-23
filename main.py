d0ad49697b50e61aa9d4de226e396a07df1ca237
Andres Figueredo <afelipefigueredo@gmail.com>
2023-03-23 10:06:22 -0500

    Funciones modificar y borrar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -9,7 +9,7 @@ app.version = '1.1.1.1'
 
 movies = [
     {
-        'id': 4,
+        'id': 1,
         'title': 'Avatar',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que",
         'year': 2009,
@@ -48,3 +48,36 @@ def get_movies_by_category(category: str, year: int):
           if i ['year'] == year:
               return i
     return ['Tu pelicula no coincide con ninguna en la lista']
+
+@app.post('/movies', tags=[movies])
+def create_movie(id: int = Body(), tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append(
+        {
+        'id': id,
+        'tittle': tittle,
+        'overview': overview,
+        'year': year,
+        'rating': rating,
+        'category': category
+    }
+    )
+    return movies
+
+@app.put('/movies', tags=[movies])
+def update_movie(id: int = Body(), tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    for i in movies:
+        if i['id'] == id:
+            i.update({
+                'year' : year,
+                'rating' : rating,
+                'category' : category
+            })
+            return i
+
+@app.delete('/movies', tags=[movies])
+def delete_movie(id: int = Body()):
+    for i in movies:
+        if i['id'] == id:
+            movies.remove(i)
+            return movies
+    return 'No se encuentra ese id'
