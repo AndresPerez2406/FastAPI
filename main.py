8af771b0401e0f40cc48005ef177e52dc293d13b
Andres Figueredo <afelipefigueredo@gmail.com>
2023-03-24 11:22:17 -0500

    Tipos de respuesta y codigos de estado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import List, Optional
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -36,7 +36,7 @@ movies = [
     {
         'id': 1,
         'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que",
+        'overview': 'En un exuberante planeta llamado Pandora viven los Navi, seres que',
         'year': 2009,
         'rating': 7.8,
         'category': 'Comedia'
@@ -44,7 +44,7 @@ movies = [
     {
         'id': 2,
         'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que",
+        'overview': "En un exuberante planeta llamado Pandora viven los Navi, seres que",
         'year': 2010,
         'rating': 7.8,
         'category': 'Acción'
@@ -55,59 +55,47 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello World<h1>')
 
-@app.get('/movies', tags=[movies])
-def get_movies():
-    return movies
+@app.get('/movies', tags=[movies], response_model= List[Movie], status_code=200)
+def get_movies() -> List[Movie]:
+    return JSONResponse(statuscode=200, codecontent=movies)
 
-@app.get('/movies/{id}', tags=[movies])
-def get_movie(id: int = Path(ge=1, le=2000)):
+@app.get('/movies/{id}', tags=[movies], response_model=Movie, status_code=200)
+def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     for i in movies:
         if i['id'] == id:
-            return i
+            return JSONResponse(statuscode=200,content=i)
     return ['No se encuentra su pelicula en la lista']
 
-@app.get('/movies/', tags=[movies])
-def get_movies_by_category(category: str = Query(min_length=5, max_length=15) ):
-    for i in movies:
-        if i ['category'] == category:
-            return i
-    return ['Tu pelicula no coincide con ninguna en la lista']
+@app.get('/movies/', tags=[movies], response_model=List[Movie], status_code=200)
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> Movie:
+    data = [ i for i in movies if i ['category'] == category]
+    return JSONResponse(statuscode=200,content=[data])
 
-@app.post('/movies', tags=[movies])
-def create_movie(movie: Movie):
-    movies.append(
-        {
-        'id': movie.id,
-        'tittle': movie.tittle,
-        'overview': movie.overview,
-        'year': movie.year,
-        'rating': movie.rating,
-        'category': movie.category
-    }
-    )
-    return movies
+@app.post('/movies', tags=[movies], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(status_code=201, content={"message": "Se registro la pelicula"})
 
-@app.put('/movies', tags=['movies'])
-def update_movie(id: int, movie: Movie):
+@app.put('/movies', tags=['movies'], response_model=dict, status_code=200)
+def update_movie(id: int, movie: Movie) -> dict:
 
     movie = [(idx) for idx, mo in enumerate(movies) if mo['id'] == id]
 
     if(len(movie) > 0):
         movies[movie[-1]] = {
-            "id": movie.id,
+            "id": id,
             "title": movie.title,
             "overview": movie.overview,
             "year": movie.year,
             "rating": movie.rating,
             "category": movie.category
         }
+        return JSONResponse(status_code=200, content={"message":'Se modifico la pelicula'})
 
-        return movies
-
-@app.delete('/movies', tags=[movies])
-def delete_movie(id: int = Body()):
+@app.delete('/movies', tags=[movies], response_model=dict, status_code=200)
+def delete_movie(id: int = Body()) -> dict:
     for i in movies:
         if i['id'] == id:
             movies.remove(i)
-            return movies
-    return 'No se encuentra ese id'
+            return JSONResponse(status_code=200, content={"message":'Se elimino la pelicula'})
+    return JSONResponse(status_code=404,content={"message":'No se encuentra ese id'})
